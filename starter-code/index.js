85df61afd427748af4b1a2481ac9caab0db75487
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-03-03 10:15:09 -0500

    Completed Lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,70 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+    
+    }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]){
+      throw new Error('OutOfBounds')
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0 ){
+      throw new Error('Empty Sorted List')
+    }
+    return this.items[this.items.length -1]
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0 ){
+      throw new Error('Empty Sorted List')
+  }
+  return this.items[0]
+}
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0 ){
+      return 0;
+  }else{
+    
+    var sum1 = 0 
+    function myFunction(item) {
+      sum1 += parseInt(item);
+    }
 
-  avg() {}
+  this.items.forEach(myFunction);
+
+  return sum1;
+}  
+  
+   
+}
+
+  avg() {
+    if(this.items.length === 0 ){
+      throw new Error('Empty Sorted List')
+  }
+  var sum1 = 0 
+    function myFunction(item) {
+      sum1 += parseInt(item);
+    }
+
+  this.items.forEach(myFunction);
+
+  return sum1 / this.items.length;
+  }
 }
 
 module.exports = SortedList;
+
+new SortedList();
